0b5286295273fe4e20ae56b91dc565f2f19e375a
kinsukSingh <kinsuksingh674@gmail.com>
2024-09-21 00:08:10 +0530

    Fixing login logout btn text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Login from './components/Login';
 import SignUp from './components/SignUp';
 import NavBar from './components/NavBar';
 import Members from './components/Members'
 import Classes from './components/Classes';
 import Account from './components/Account';
 import Pricing from './components/Pricing';
-import Test from './components/Test';
 import { useState } from 'react';
 
 
@@ -23,16 +21,14 @@ function App() {
 
   return (
     <Router>
-      <NavBar onLogout={handleLogout} isOwner={userType}/>
+      <NavBar settingUserType={(type)=> setUserType(type)} onLogout={handleLogout} isOwner={userType}/>
       <Routes>
-        <Route path='/login' element={<Login/>}></Route>
         <Route path='/signup' element={<SignUp/>}></Route>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/account' element={<Account/>}></Route>
         <Route path='/members' element={<Members isOwner={userType} />}></Route>
         <Route path='/classes' element={<Classes isOwner={userType}/>}></Route>
-        <Route path='/account' element={<Account/>}></Route>
         <Route path='/pricing' element={<Pricing isOwner={userType}/>}></Route>
-        <Route path='/test' element={<Test settingUserType={(type)=> setUserType(type)}/>}></Route>
       </Routes>
     </Router>
   );
--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,48 +1,61 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import { Link,useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { LuDumbbell } from "react-icons/lu";
+import { Navbar, Nav, Button, Container } from 'react-bootstrap';
+import LoginModal from '../LoginModal';
 
-function NavBar({onLogout, isOwner }) {
+function NavBar({onLogout, isOwner, settingUserType }) {
+  const [showModal, setShowModal] = useState(false);
+  const [isLogin, setIsLogin] = useState(false)
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    // Perform logout action here
+    setIsLogin(false)
     onLogout();
-    navigate('/test'); // Redirect to login page after logout
+    navigate('/'); // Redirect to home page after logout
+  };
+
+  const handleLoginClick = () => {
+    setShowModal(true); // Show modal when Login is clicked
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false); // Close modal
   };
 
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark" >
-      <Container>
-        <Navbar.Brand as={Link} to="/">Bros<LuDumbbell />Gym</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/classes">Classes</Nav.Link>
-            <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
-            <Nav.Link as={Link} to="/account">Account</Nav.Link>
-            {isOwner && <Nav.Link as={Link} to="/members">Members</Nav.Link>}
-          </Nav>
-          <Nav>
-            <Button variant={true ? "outline-primary":"outline-danger"} onClick={handleLogout}>
-              {true ? "Login" : "LogOut"}
-            </Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark" >
+        <Container>
+          <Navbar.Brand as={Link} to="/">Bros<LuDumbbell />Gym</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/classes">Classes</Nav.Link>
+              {isOwner && <Nav.Link as={Link} to="/members">Members</Nav.Link>}
+              <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
+              <Nav.Link as={Link} to="/account">Account</Nav.Link>
+            </Nav>
+            <Nav>
+              <Button
+                variant={isLogin ? "outline-danger" : "outline-primary"}
+                onClick={isLogin ? handleLogout : handleLoginClick}
+              >
+                {isLogin ? "Logout" : "Login"}
+              </Button>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Modal for login */}
+      <LoginModal settingIsLogin={(value)=>{setIsLogin(value)}} show={showModal} handleClose={handleCloseModal} settingUserType={(type) => settingUserType(type)} />
+    </>
   );
 }
 
 export default NavBar;
-
-
-
-
 
 
 
